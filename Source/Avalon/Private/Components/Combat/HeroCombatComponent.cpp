4172ae97c7a88dfb6153a9d541c4c4278339273c
Sam Hill <sjhill1995@gmail.com>
2025-01-28 12:25:15 +0000

    Section 4: Hero Combat 79. Camera Shake

--- Source/Avalon/Private/Components/Combat/HeroCombatComponent.cpp
+++ Source/Avalon/Private/Components/Combat/HeroCombatComponent.cpp
@@ -36,9 +36,12 @@ void UHeroCombatComponent::OnHitTargetActor(AActor* HitActor)
 	Data.Target = HitActor;
 	
 	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwningPawn(), AvalonGameplayTags::Shared_Event_MeleeHit, Data);
-	}
+
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwningPawn(), AvalonGameplayTags::Player_Event_HitPause, FGameplayEventData());
+}
 
 void UHeroCombatComponent::OnWeaponPulledFromTargetActor(AActor* InteractedActor)
 {
 	//Debug::Print(GetOwningPawn()->GetActorNameOrLabel() + TEXT(" pulled from ") + InteractedActor->GetActorNameOrLabel(), FColor::Red);
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(GetOwningPawn(), AvalonGameplayTags::Player_Event_HitPause, FGameplayEventData());
 }
